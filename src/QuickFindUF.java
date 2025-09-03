d04e8e2a729ad28a7d2a1ccfcf088c1cfe0a44ff
namnkahn1607 <nlnkandthebois@gmail.com>
2025-09-03 23:43:38 +0700

    algs4 first commit

---
+++ src/QuickFindUF.java
@@ -0,0 +1,42 @@
+/* Quick Find
+ * N: number of elements
+ * K: number of method calls
+ * initialize: O(N)
+ * union: O(N.K) - not fast
+ * connected: O(K)
+ * */
+
+import edu.princeton.cs.algs4.StdIn;
+
+public class QuickFindUF extends AbstractUF {
+
+    public QuickFindUF(int N) {
+        super(N);
+    }
+
+    @Override
+    public void union(int p, int q) {
+        if (connected(p, q)) return;
+
+        final int IDp = id[p];
+        final int IDq = id[q];
+
+        for (int i = 0; i < size; ++i) {
+            if (id[i] == IDp)
+                id[i] = IDq;
+        }
+    }
+
+    @Override
+    public int root(int p) {
+        validateVal(p);
+
+        return id[p];
+    }
+
+    public static void main(String[] args) {
+        final int N = StdIn.readInt();
+        QuickFindUF uf = new QuickFindUF(N);
+        AbstractUF.run(uf);
+    }
+}
